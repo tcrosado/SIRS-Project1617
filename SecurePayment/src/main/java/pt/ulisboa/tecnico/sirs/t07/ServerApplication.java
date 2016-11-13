debf8af081d2142e6cab691aad4ff9d3122930fa
joaosousa91 <joaopedro.csousa@gmail.com>
2016-11-13 15:19:47 +0000

    Serviço para ver o saldo
    http://localhost:4567/conta/NL73INGB0698363980 em que o ultimo elemento
    do url é o NIB

--- SecurePayment/src/main/java/pt/ulisboa/tecnico/sirs/t07/ServerApplication.java
+++ SecurePayment/src/main/java/pt/ulisboa/tecnico/sirs/t07/ServerApplication.java
@@ -4,45 +4,47 @@ import pt.ulisboa.tecnico.sirs.t07.data.ContaData;
 import pt.ulisboa.tecnico.sirs.t07.data.CustomerData;
 
 import java.io.IOException;
+import java.util.Vector;
+
 
 import static spark.Spark.*;
 
-	
+
 
 /**
  * Created by trosado on 03/11/16.
  */
 public class ServerApplication {
 
-    public static void main(String[] args) throws IOException {
-       /* DatagramSocket serverSocket = new DatagramSocket(9876);
+	public static void main(String[] args) throws IOException {
+		/* DatagramSocket serverSocket = new DatagramSocket(9876);
         byte[] receiveData = new byte[1024];
         byte[] sendData = new byte[1024];
         while(true)
         { */
-            /**
-            * TODO
-            *  - Threads (cada cliente)
-            *  - JDBC interface
-            *  - enviar resposta até receber confirmação
-            *  - tamanho maximo UDP
-            * */
+		/**
+		 * TODO
+		 *  - Threads (cada cliente)
+		 *  - JDBC interface
+		 *  - enviar resposta até receber confirmação
+		 *  - tamanho maximo UDP
+		 * */
 
 
-            /**
-             * TODO
-             *  Operações
-             *  - Transferência
-             *  - Histórico
-             *  - Saldo
-             *
-             *  Respostas
-             *  - Saldo Insuficiente
-             *  - Operação repetida
-             *  - Confirmação
-             *  - Mensagem de Bloqueio
-             */
-    /*
+		/**
+		 * TODO
+		 *  Operações
+		 *  - Transferência
+		 *  - Histórico
+		 *  - Saldo
+		 *
+		 *  Respostas
+		 *  - Saldo Insuficiente
+		 *  - Operação repetida
+		 *  - Confirmação
+		 *  - Mensagem de Bloqueio
+		 */
+		/*
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
             String sentence = new String( receivePacket.getData());
@@ -59,14 +61,23 @@ public class ServerApplication {
                     new DatagramPacket(sendData, sendData.length, IPAddress, port);
             serverSocket.send(sendPacket);
         }
-        */
-        CustomerData cd = new CustomerData();
-        System.out.print(cd.ibanExists("NL73INGB0698363980"));
-        cd.close();
-        
-        ContaData contaD = new ContaData();
-        
-        get("/contas", (req, res) -> contaD.getContas());
+		 */
+		CustomerData cd = new CustomerData();
+		System.out.print(cd.ibanExists("NL73INGB0698363980"));
+		cd.close();
 
-    }
+		ContaData contaD = new ContaData();
+
+		get("/contas", (req, res) -> contaD.getContas());
+
+		get("/conta/:iban", (req, res) -> {
+
+			String iban = req.params(":iban");
+			Vector<Float> saldo = contaD.getSaldoFromIBAN(iban);
+			return saldo;
+
+
+		});
+
+	}	
 }
