c11cc6771614a60de2547aebd5744fe139efa7b6
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2016-11-16 16:09:52 +0000

    Added partial parser

--- SecurePayment/src/main/java/pt/ulisboa/tecnico/sirs/t07/service/PacketParserService.java
+++ SecurePayment/src/main/java/pt/ulisboa/tecnico/sirs/t07/service/PacketParserService.java
@@ -3,7 +3,11 @@ package pt.ulisboa.tecnico.sirs.t07.service;
 import pt.ulisboa.tecnico.sirs.t07.service.dto.OperationData;
 
 import java.net.DatagramPacket;
+import java.nio.ByteBuffer;
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.UUID;
 
 /**
  * Created by tiago on 12/11/2016.
@@ -16,21 +20,22 @@ public class PacketParserService extends AbstractService {
     public  PacketParserService(DatagramPacket packet) throws Exception{
         setPacket(packet);
         resultData = null;
-        System.out.print("yep");
     }
 
     @Override
     void dispatch() {
        System.out.println(this.packet.getData().length);
-        byte[] uid = Arrays.copyOf(this.packet.getData(),16);
-        System.out.println(Arrays.toString(uid));
-       byte[] timestamp = Arrays.copyOfRange(this.packet.getData(),16,32);
-       byte[] hash = Arrays.copyOfRange(this.packet.getData(),32,48);
-       byte[] opbyte = Arrays.copyOfRange(this.packet.getData(),48,64);
-        System.out.print(Arrays.toString(opbyte));
-        System.out.print("got it");
-        char operation = (char)(((opbyte[0]&0x00FF)<<8) + (opbyte[1]&0x00FF));
-        System.out.print("a:"+operation);
+        byte[] uidbyte = Arrays.copyOf(this.packet.getData(),36);
+        byte[] timestampbyte = Arrays.copyOfRange(this.packet.getData(),36,49);
+        byte[] hashbyte = Arrays.copyOfRange(this.packet.getData(),49,62);
+        byte[] opbyte = Arrays.copyOfRange(this.packet.getData(),62,63);
+
+        UUID tuid = this.toUUID(uidbyte);
+        Timestamp time = this.toTimeStamp(timestampbyte);
+        String hash = hashbyte.toString();
+        char operation = (char)(opbyte[0]);
+
+
        switch (operation){
 
            case 'S':
@@ -40,6 +45,7 @@ public class PacketParserService extends AbstractService {
            case 'T':
                //TODO definir serviço de tranferencia
                System.out.println("Transferencias");
+               //this.resultData = new OperationData(tuid,time,new TransferService());
                 break;
            case 'H':
                //TODO definir serviço de historico
@@ -48,7 +54,6 @@ public class PacketParserService extends AbstractService {
            default:
                //TODO enviar excecao
                System.out.println("Throw");
-
        }
 
     }
@@ -67,5 +72,16 @@ public class PacketParserService extends AbstractService {
     }
 
 
+
+    private UUID toUUID(byte[] uidbyte){
+        String uids = new String(uidbyte);
+        return UUID.fromString(uids);
+    }
+
+
+    private Timestamp toTimeStamp(byte[] timestamp){
+        ByteBuffer bb = ByteBuffer.wrap(timestamp);
+        return new Timestamp(bb.getLong());
+    }
 }
 
